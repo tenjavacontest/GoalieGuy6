37d6756a0c5f3d6c477a0f0224727be07aa31f02
GoalieGuy6 <goalieguy6@gmail.com>
2013-12-06 15:17:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+ten.java - GoalieGuy6
+=====================
+
+Compilation
+-----------
+* Install [Maven 3](http://maven.apache.org/download.cgi)
+* Checkout this repo
+* Run `mvn clean install`
---
+++ src/main/java/com/goalieguy6/tenjava/TenJavaPlugin.java
@@ -0,0 +1,17 @@
+package com.goalieguy6.tenjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TenJavaPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+}
